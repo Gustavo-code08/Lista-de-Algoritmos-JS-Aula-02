b913918d257fb5a3ddecc0cef33857e9f499edb1
Gustavo-code08 <gustavooj08@gmail.com>
2026-06-17 15:44:58 -0300

    Delete desafio08.js

--- desafio08.js
+++
@@ -1,8 +0,0 @@
-let num = prompt("Digite um número: ");
-
-if(num >= 10 && num <= 50){
-    console.log (`O número ${num} está entre o intervalo de 10 e 50.`)
-}
-else{
-    console.log (`O número ${num} não está entre o intervalo de 10 e 50.`)
-}
